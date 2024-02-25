dcffb5ca0d5bdddf8f34c4490ff1017d359befa0
izzeww <izzew@hemmadatorn>
2024-02-25 22:27:49 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 
 
+
     const getData = async () => {
-        const res = await fetch('https://query1.finance.yahoo.com/v7/finance/quote?&symbols=GME,^DJI,^GSPC,^IXIC,^RUT&fields=currency,fromCurrency,toCurrency,exchangeTimezoneName,exchangeTimezoneShortName,gmtOffSetMilliseconds,regularMarketChange,regularMarketChangePercent,regularMarketPrice,regularMarketTime,preMarketTime,postMarketTime,extendedMarketTime&crumb=6Vjd51pot.2&formatted=false&region=US&lang=en-US');
+        const res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&timezone=Europe%2FBerlin');
         const jsonData = res.json(); 
     
         console.log(jsonData);
@@ -14,4 +15,3 @@
 <h1>Welcome to SvelteKit yahooolmao</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={getData}>Get Data</button>
-
