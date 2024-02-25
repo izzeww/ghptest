a1cd566b7f4f97999edc51356d4d034fdb9c9768
izzeww <izzew@hemmadatorn>
2024-02-25 22:34:20 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,6 @@
     
 </script>
 
-<h1>Welcome to SvelteKit yahooolmao</h1>
+<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={getData}>Get Data</button>
